93f2fe14b23442d2357795b5e21f6a6ca9671dea
jubna <jubi912@gmail.com>
2021-08-04 23:38:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component
+{
+  constructor(props){
+    super(props)
+    // this.state = {value: ''};
+
+   
+    this.state={
+      items:[],
+      loading:false,
+      age:[]
+    }
+      this.handleChange = this.handleChange.bind(this);
+     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  componentDidMount(){
+    var value = 4;
+    let inputStyle = {
+      backgroundColor:""
+    };
+   
+    fetch(`https://randomuser.me/api?results=${value}`)
+    .then((response)=>
+      response.json())
+    .then((response)=>{
+      let name=response.results.map((person)=>{
+        let age=person.dob.age;
+        
+
+      this.setState({
+        items:response.results,
+        loading:true,
+        age:age
+      })
+    })
+    })
+    
+  }
+  render(){
+    var {age,items,loading}=this.state;
+    let inputStyle = {
+      backgroundColor:""
+    };
+  
+    if(age >=0 && age<=18) {
+      inputStyle = {
+        backgroundColor:"blue"
+      }
+  
+    }
+    else if(age>=19 && age<=36){
+      inputStyle = {
+        backgroundColor:"green"
+      }
+    
+    }
+    else{
+      inputStyle = {
+        backgroundColor:"red"
+      }
+      
+    }
+       
+
+  
+ 
+    return(
+      <div className="container">
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          Number of Users: 
+           <input type="text" id="value" value={this.state.value} onChange={this.handleChange} /> 
+        
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+        {items.map(item=>(<div className="card" style={inputStyle} >
+           <div class="card-body">
+        <h5 className="card-title">{item.name.first}{item.name.last}</h5> 
+        <p class="card-text"  id="age">{item.dob.age}</p>
+        </div>
+        </div>))}
+      </div>
+    )
+  }
 }
-
 export default App;
